a3d7913de2ae81ac0345cac33f984100d8ea9055
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-14 14:48:57 -0500

    adds stock photos

--- print-recipe.js
+++ print-recipe.js
@@ -3,12 +3,20 @@ const printRecipe = function(recipe) {
     const img = document.createElement('img')
     li.innerText = `${recipe.text}`;
     img.src = `${recipe.src}`;
-    const ul = document.querySelector('.recipe-list');
+    const ol = document.querySelector('.recipe-list');
   
-    ul.appendChild(li);
-    ul.appendChild(img)
+    ol.appendChild(li);
+    ol.appendChild(img)
     li.classList.add('recipe-item');
     img.classList.add('image-item')
     // li.id = recipe.id.toString();
     
   }
+
+  const printCurrent = function(recipes) {
+    for (const recipe of recipes) {
+      printRecipe(recipe);
+    }
+  }
+  
+  printCurrent(recipes);
--- recipes.js
+++ recipes.js
@@ -1,38 +1,30 @@
 const recipes = [
     {
-      text: 'Christmas shopping',
-      image:'',
+      text: 'Lasagna',
+      src:'https://images.media-allrecipes.com/userphotos/720x405/3359675.jpg',
+      link:'https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/',
       id: 0,
     
     },
     {
-      text: `make doctor's appointment`,
-      image:'',
+      text: 'Chicken Picata',
+      src:'https://www.jessicagavin.com/wp-content/uploads/2015/09/chicken-piccata-8-1200.jpg',
+      link: 'https://www.foodnetwork.com/recipes/giada-de-laurentiis/chicken-piccata-recipe2-1913809',
       id: 1,
       
     },
     {
-      text: 'binge watch The Office',
-      image:'',
+      text: 'Banh Mi',
+      src:'https://www.seriouseats.com/recipes/images/2015/02/20150216-grilled-tofu-banh-mi-recipe-vegan-12-1500x1125.jpg',
+      link: 'https://www.allrecipes.com/recipe/187342/banh-mi/',
       id: 2,
     
     },
     {
-      text: 'research dinosaurs',
-      image:'',
+      text: 'Fish Tacos',
+      src:'https://www.spendwithpennies.com/wp-content/uploads/2013/02/Easy-Fish-Tacos-24.jpg',
+      link: 'https://www.spendwithpennies.com/fish-tacos-recipe-onion-slaw/',
       id: 3,
-    
-    },
-    {
-      text: 'fire Colin',
-      image:'',
-      id: 4,
-    },
-    {
-      text: 'start novel',
-      image:'',
-      id: 5,
-     
     },
     
   ]
