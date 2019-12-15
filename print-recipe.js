f6dd06cda1405c322fa8debcd4637b793f02df36
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-14 19:48:26 -0500

    adds link to site

--- print-recipe.js
+++ print-recipe.js
@@ -1,14 +1,20 @@
 const printRecipe = function(recipe) {
     const li = document.createElement('li');
+    const link = document.createElement('a');
     const img = document.createElement('img')
     const checkBox = document.createElement('input')
 
     li.innerText = `${recipe.text}`;
     img.src = `${recipe.src}`;
+    link.href = `${recipe.link}`;
     checkBox.type = 'checkbox'
 
     const ol = document.querySelector('.recipe-list');
     
+    img.onclick = function() {
+      window.location.href = recipe.link;
+  };
+
     ol.appendChild(li);
     ol.appendChild(checkBox);
     ol.appendChild(img);
