70942db6cd09855d7d640692b1ce929a596e3a16
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-14 20:54:04 -0500

    fixes links

--- add-recipe.js
+++ add-recipe.js
@@ -1,7 +1,8 @@
-const addRecipe = (text, src) => {
+const addRecipe = (text, src, link) => {
     let thisRecipe = {
       text: text,
       src: src,
+      link:link,
       id: recipes.length,
     }
     currentRecipes.push(thisRecipe);
@@ -12,11 +13,13 @@ const addRecipe = (text, src) => {
 
 const nameInput = document.querySelector('.recipe-input');
 const srcInput = document.querySelector('.image-input')
+const linkInput = document.querySelector('.link-input')
 
 document.querySelector('.add-recipe').addEventListener('click',function(){
-    printRecipe(addRecipe(nameInput.value, srcInput.value));
+    printRecipe(addRecipe(nameInput.value, srcInput.value, linkInput.value));
     nameInput.value = '';
     srcInput.value = '';
+    linkInput.value = '';
 
   })
 
